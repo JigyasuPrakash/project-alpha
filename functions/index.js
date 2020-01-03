93856c9bb7b7cc124537ec3f536328a5f39643cb
ashleshmahule15 <ashleshmahule15@gmail.com>
2020-01-03 22:20:29 +0530

    data retrieval from firebase added

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,21 @@
-const functions = require('firebase-functions');
+var firebase=require('firebase');
 
-const admin = require('firebase-admin');
-admin.initializeApp();
+var firebaseConfig = {
+    apiKey: "AIzaSyCx3lQ3Oe4oKrJCdTcp4CKlDeKiRRxbdWc",
+    authDomain: "project-tnp-be0af.firebaseapp.com",
+    databaseURL: "https://project-tnp-be0af.firebaseio.com",
+    projectId: "project-tnp-be0af",
+    storageBucket: "project-tnp-be0af.appspot.com",
+    messagingSenderId: "518046381835",
+    appId: "1:518046381835:web:f87059d9fdfc1fe0f4140e",
+    measurementId: "G-R71GXZPYXL"
+};
 
-
-exports.retreiveData=functions.https.onRequest((request,response) => {
-    var db=admin.database();
-    var ref=db.ref().child('StudentDB').child('Student1');
-    console.log('hello');
-    
-    ref.on('value', function(data) {
-        console.log(data);
-    });
-    
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.database().ref().child('StudentDB').on('child_added',function(object) {
+    console.log(object.val());
 });
-
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
