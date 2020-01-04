471ce103d0311104fcb8cf44cc36cf56e737229e
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-01-04 22:57:49 +0530

    update #7

---
+++ public/assets/scripts/addStudent.js
@@ -0,0 +1,19 @@
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        console.log(user.uid);
+    } else {
+        window.location.href = "./login.html";
+    }
+});
+
+$(document).ready(function(){
+    console.log("Document is ready")
+    $('#userName').text(localStorage.getItem('userNameRecieved'));
+    $('#userMail').text(localStorage.getItem('userMailRecieved'));
+});
+
+function logOut() {
+    console.log("Signout process started")
+    auth.signOut();
+    localStorage.clear;
+}
