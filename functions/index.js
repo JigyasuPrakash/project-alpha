6039978f24003bd8108ef1d20362a37c66d9d4bd
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-01-16 00:28:08 +0530

    getDataById added and admin SDK configured

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,44 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
-admin.initializeApp();
+/* 
+//project-tnp-be0af-firebase-adminsdk-gbpvv-65c1a43930.json
+    this file might be missing in your git pull because this file consists of private access key..
+    which cannot be shared with any one.. to get your private key head over to firebase -> project setting
+    scroll down and click on generate private key and paste that file inside this project on the path
+    TNP-App -> functions..
 
+*/
+const serviceAccount = require('./project-tnp-be0af-firebase-adminsdk-gbpvv-65c1a43930.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
 
-exports.retreiveData=functions.https.onRequest((request,response) => {
-    var db=admin.database();
-    var ref=db.ref().child('StudentDB').child('Student1');
-    console.log('hello');
-    
-    ref.on('value', function(data) {
-        console.log(data);
-    });
-    
+exports.getDataById = functions.https.onRequest((request, response) => {
+    //Id collection from request is pending......
+    console.log("Stating here");
+    db.collection('Student-data').doc('rashmi').get()
+        .then(doc => {
+            //Handle data recieved from database here and feed into the response method
+            response.status(200).json({
+                name: doc.data().firstName,
+                email: doc.data().email,
+                message: 'Nice.. it is working!!'
+            });
+        })
+        .catch(err => {
+            response.status(404).json({
+                message: 'Document not found'
+            });
+            process.exit();
+        })
 });
 
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+/*
+* const obj = JSON.parse(input text);
+* const doc = {
+    name: obj.name,
+    branch: obj.branch
+    };
+    return db.collec
+*/
