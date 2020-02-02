5007be5fc538c097634a4c32c4932957b085c2be
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-02-02 21:19:52 +0530

    update #19

--- routes/dataFetch.js
+++ routes/dataFetch.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
