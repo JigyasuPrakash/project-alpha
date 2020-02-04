0efeb2b2a9708903ac910d6d3bdd07dab8593c4f
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-02-04 23:33:20 +0530

    filter json response fixed

--- routes/dataFetch.js
+++ routes/dataFetch.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cors = require('cors');
 const config = require('../config/database');
 const passport = require('passport');
-var mongoClient=require('mongodb').MongoClient;
+var mongoClient = require('mongodb').MongoClient;
 
 // this endpoint would be accessed by /api/admin/fetch/data/{pathname}
 // and requires JSON WEB TOKENS for authentication..
@@ -21,66 +21,61 @@ router.get('/{pathname}', passport.authenticate('jwt', {
 });
 */
 
-var searchResult=new Set();
-
 router.get('/searchStudent', (req, res) => {
     //handle request
-    
-    var filterData=req.query;
-    var tosend={
-        searchResult: null
+
+    var filterData = req.query;
+    var tosend = [];
+    var branch = filterData.Branch;
+    var gender = filterData.Gender;
+    var cgpa = filterData.CGPA;
+    if (cgpa == "" || cgpa == null) {
+        cgpa = 0;
     }
-    var branch=filterData.Branch;
-    var gender=filterData.Gender;
-    var cgpa=filterData.CGPA;
-    if(cgpa=="" || cgpa==null) {
-        cgpa=0;
-    }
-   
-    mongoClient.connect(config.cluster, function(error, database) {
-        if(error) {
+
+    mongoClient.connect(config.cluster, function (error, database) {
+        if (error) {
             console.log('error');
             throw error;
         }
 
-        var dbObject_personal=database.db('student_details');
+        var dbObject_personal = database.db('student_details');
 
         dbObject_personal.collection('student_personal_details').find({
-                Branch: {$in: branch}
-            }).toArray(function(error, result) {
-            if(error) {
+            Branch: { $in: branch }
+        }).toArray(function (error, result) {
+            if (error) {
                 console.log('error');
                 throw error;
             }
             //console.log(result);
-            
-            
+
+
             result.forEach(element => {
-                if(element.CGPA>=cgpa) {
-                    searchResult.add(element);
+                if (element.CGPA >= cgpa) {
+                    tosend.push(element);
                 }
             });
-            if(gender!=null) {
-                searchResult.clear();
+            if (gender != null) {
+                tosend = []
                 result.forEach(element => {
-                    if(gender.includes(element.Gender) && element.CGPA>=cgpa) {
-                        searchResult.add(element);
+                    if (gender.includes(element.Gender) && element.CGPA >= cgpa) {
+                        tosend.push(element);
                     }
                 });
             }
-            console.log(searchResult);
-            tosend['searchResult']=searchResult;
-            if(searchResult.size!=0) {
-                console.log(searchResult.size);
-                res.json(tosend);
+            console.log(tosend);
+            console.log(tosend.length)
+            if (tosend.size != 0) {
+                res.json(JSON.stringify(tosend));
             }
-            searchResult.clear();
+            tosend = [];
         });
     });
 
     //send response
-    res.setHeader('Content-Type', 'text/html');
-    
+    res.setHeader('Content-Type', 'application/json');
+
 });
 
 module.exports = router;
