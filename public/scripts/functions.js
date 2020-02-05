b9ad4c37fb8b80621c8b8bb7ec4e1e2e5589dd9f
ashleshmahule <ashleshmahule15@gmail.com>
2020-02-06 00:32:41 +0530

    analytics

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -19,7 +19,8 @@ $(document).ready(function () {
             if (window.location.pathname === "/dashboard/student/result") {
                 getData();
             } else if (window.location.pathname === "/dashboard/student") {
-                getStudentAnalytics()
+                getStudentAnalytics();
+                getBranchAnalytics();
             }
         },
     });
@@ -443,6 +444,93 @@ function getStudentAnalytics() {
                         ],
                         borderWidth: 1
 
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+    });
+}
+
+function getBranchAnalytics() {
+    var branches=['Computer Science and  Engineering','Electronics and Communication Engineering','Electronics Engineering','Electrical Engineering','Civil Engineering','Information Technology','Electronics Design Technology','Industrial Engineering'];
+    $.ajax({
+        method: "GET",
+        datatype: "json",
+        data: {Branch: branches},
+        url: "/api/fetch/data/getBranchAnalytics",
+        error: function () {
+            console.log('error occured');
+        },
+        success: function (data) {
+            data = JSON.parse(data);
+            console.log(data);
+
+            var ctx = document.getElementById('avgBranchwiseAnalytics').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['CSE','EC','EN','ELEC','CIV','IT','EDT','IND'],
+                    datasets: [{
+                        label: 'Branch-wise Package',
+                        data: data[1],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)'
+                        ],
+                        borderWidth: 1,
+                        order: 1,
+                        maxBarThickness: 8
+                    },{
+                    label: 'Branch-wise CGPA',
+                        data: data[0],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)'
+                        ],
+                        borderWidth: 1,
+                        order: 2,
+                        maxBarThickness: 8
                     }]
                 },
                 options: {
--- routes/dataFetch.js
+++ routes/dataFetch.js
@@ -159,8 +159,8 @@ router.get('/getStudentAnalytics', (req,res) => {
             throw error;
         }
 
-        var dbObject_personal = database.db('student_details');
-        dbObject_personal.collection('student_current_academic_details').find({
+        var dbObject = database.db('student_details');
+        dbObject.collection('student_current_academic_details').find({
             _id: student
         }).toArray(function(error, result) {
             if (error) {
@@ -177,11 +177,100 @@ router.get('/getStudentAnalytics', (req,res) => {
                 toSend.push(element.Sem7SGPA);
                 toSend.push(element.Sem8SGPA);
             });
-            console.log(toSend);
+            //console.log(toSend);
             res.json(JSON.stringify(toSend));
         });
     });
 
+});
+
+router.get('/getBranchAnalytics', (req,res) => {
+    var branch=req.query.Branch;
+    console.log(branch)
+    var CGPA=[],PKG=[];
+    var cg1=0,cg2=0,cg3=0,cg4=0,cg5=0,cg6=0,cg7=0,cg8=0;
+    var c1=c2=c3=c4=c5=c6=c7=c8=0;
+    var pkg1=pkg2=pkg3=pkg4=pkg5=pkg6=pkg7=pkg8=0;
+    var toSend=[];
+
+    mongoClient.connect(config.cluster, function (error, database) {
+        if (error) {
+            console.log('error');
+            throw error;
+        }
+
+        var dbObject_personal = database.db('student_details');
+        dbObject_personal.collection('student_personal_details').find({
+            Branch: {$in: branch}
+        }).toArray(function(error, result) {
+            result.forEach(element => {
+                if(element.Branch==='Computer Science and  Engineering') {
+                    cg1+=element.CGPA;
+                    c1++;
+                    //pkg1+=element.package_offered;
+                }
+                if(element.Branch==='Electronics and Communication Engineering') {
+                    cg2+=element.CGPA;
+                    c2++;
+                    //pkg2+=element.package_offered;
+                }
+                if(element.Branch==='Electronics Engineering') {
+                    cg3+=element.CGPA;
+                    c3++;
+                    //pkg3+=element.package_offered;
+                }
+                if(element.Branch==='Electrical Engineering') {
+                    cg4+=element.CGPA;
+                    c4++;
+                    //pkg4+=element.package_offered;
+                }
+                if(element.Branch==='Civil Engineering') {
+                    cg5+=element.CGPA;
+                    c5++;
+                    //pkg5+=element.package_offered;
+                }
+                if(element.Branch==='Information Technology') {
+                    cg6+=element.CGPA;
+                    c6++;
+                    //pkg6+=element.package_offered;
+                }
+                if(element.Branch==='Electronics Design Technology') {
+                    cg7+=element.CGPA;
+                    c7++;
+                    //pkg7+=element.package_offered;
+                }
+                if(element.Branch==='Industrial Engineering') {
+                    cg8+=element.CGPA;
+                    c8++;
+                    //pkg8+=element.package_offered;
+                }
+            });
+            CGPA[0]=(parseFloat((cg1/c1).toFixed(2)));
+            CGPA[1]=(parseFloat((cg2/c2).toFixed(2)));
+            CGPA[2]=(parseFloat((cg3/c3).toFixed(2)));
+            CGPA[3]=(parseFloat((cg4/c4).toFixed(2)));
+            CGPA[4]=(parseFloat((cg5/c5).toFixed(2)));
+            CGPA[5]=(parseFloat((cg6/c6).toFixed(2)));
+            CGPA[6]=(parseFloat((cg7/c7).toFixed(2)));
+            CGPA[7]=(parseFloat((cg8/c8).toFixed(2)));
+
+            //PKG[0]=(parseFloat((pkg1/c1).toFixed(2)));
+            //PKG[1]=(parseFloat((pkg2/c2).toFixed(2)));
+            //PKG[2]=(parseFloat((pkg3/c3).toFixed(2)));
+            //PKG[3]=(parseFloat((pkg4/c4).toFixed(2)));
+            //PKG[4]=(parseFloat((pkg5/c5).toFixed(2)));
+            //PKG[5]=(parseFloat((pkg6/c6).toFixed(2)));
+            //PKG[6]=(parseFloat((pkg7/c7).toFixed(2)));
+            //PKG[7]=(parseFloat((pkg8/c8).toFixed(2)));
+
+            toSend.push(CGPA);
+            toSend.push(PKG);
+
+            //console.log(CGPA);
+            res.json(JSON.stringify(toSend));
+        });
+        
+    });
 });
 
 module.exports = router;
