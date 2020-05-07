fe6ea733eca79a1c1bf318d2feace1ba005472de
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-05-07 14:12:29 +0530

    cleanup

--- config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    database: "mongodb+srv://tnp-admin:adminLog@my-tnp-database-daj6w.mongodb.net/User-Accounts?retryWrites=true&w=majority",
-    secret: "ThisiSsecretkeY",
-    cluster: "mongodb+srv://tnp-admin:adminLog@my-tnp-database-daj6w.mongodb.net/test"
-}
