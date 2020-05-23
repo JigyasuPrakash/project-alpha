741898466ccda600058ee38bddd5dc830298b28a
gauravv-shukla <shuklagaurav557@gmail.com>
2020-05-23 10:18:55 +0530

    Js files and css folder created

--- routes/page/pageRouter.js
+++ routes/page/pageRouter.js
@@ -8,4 +8,20 @@ router.get('/test', (req, res) => {
     res.send('Page test woking');
 })
 
+router.get('/homepage', (req, res) => {
+    res.render('homepage.html');
+})
+
+router.get('/developer', (req, res) => {
+    res.render('developer.html');
+})
+
+router.get('/login', (req, res) => {
+    res.render('login.html');
+})
+
+router.get('/signup', (req, res) => {
+    res.render('signup.html');
+})
+
 module.exports = router;
